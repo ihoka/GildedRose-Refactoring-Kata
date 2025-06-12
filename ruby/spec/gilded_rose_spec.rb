969c00e035579375384861a3788a10c9bbad623e
Istvan Hoka <istvan.hoka@gmail.com>
2025-06-12 07:50:30 +0100

    Tweak rspec config

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,11 +1,9 @@
-require 'rspec'
+require_relative 'spec_helper'
 
-require_relative "../gilded_rose"
-
-describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "foo"
+RSpec.describe GildedRose do
+  it 'does not change the name' do
+    items = [Item.new('foo', 0, 0)]
+    GildedRose.new(items).update_quality
+    expect(items[0].name).to eq 'foo'
   end
 end
--- ruby/spec/gold_master_spec.rb
+++ ruby/spec/gold_master_spec.rb
@@ -1,26 +1,26 @@
-describe GildedRose do
-  describe 'Gold Master' do
-    ITERATIONS = 100
+require_relative 'spec_helper'
 
-    it 'is consistent with starting version' do
-      fixture_path = File.expand_path('../gold_master.txt', __dir__)
-      fixture_generator_path = File.expand_path('../texttest_fixture.rb', __dir__)
+RSpec.describe 'Gold Master' do
+  ITERATIONS = 100
 
-      actual_lines = `ruby #{fixture_generator_path} #{ITERATIONS}`.split("\n")
-      expected_lines = File.read(fixture_path).split("\n")
+  it 'is consistent with starting version' do
+    fixture_path = File.expand_path('../gold_master.txt', __dir__)
+    fixture_generator_path = File.expand_path('../texttest_fixture.rb', __dir__)
 
-      line_count = [actual_lines.count, expected_lines.count].max
+    actual_lines = `ruby #{fixture_generator_path} #{ITERATIONS}`.split("\n")
+    expected_lines = File.read(fixture_path).split("\n")
 
-      line_count.times do |num|
-        actual = actual_lines[num].to_s
-        expected = expected_lines[num]
+    line_count = [actual_lines.count, expected_lines.count].max
 
-        expect(actual).to eq(expected), <<~TXT
-          LINE #{num}:
-            expected: #{expected.inspect}
-            got:      #{actual.inspect}
-        TXT
-      end
+    line_count.times do |num|
+      actual = actual_lines[num].to_s
+      expected = expected_lines[num]
+
+      expect(actual).to eq(expected), <<~TXT
+        LINE #{num}:
+          expected: #{expected.inspect}
+          got:      #{actual.inspect}
+      TXT
     end
   end
 end
